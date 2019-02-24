10a052fcd9c75780a62705d5c031886a6df7acc7
Brian Summerfield <brianjsummerfield@gmail.com>
2019-02-24 12:07:35 -0600

    added fortune_method

--- app/controllers/api/sample_pages_controller.rb
+++ app/controllers/api/sample_pages_controller.rb
@@ -1,6 +1,8 @@
 class Api::SamplePagesController < ApplicationController
 
   def fortune_method
+    @message = "happy day"
+    render 'fortune.json.jbuilder'
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+get '/fortune_url' => 'api/sample_pages#fortune_method'
+
 end
